1c52f47b1dff0bd4274c455c11bda98afe379157
Paul Cortes <paulkerg@gmail.com>
2020-04-20 13:44:31 -0500

    correcion por typo en router era route para la vista posts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -10,7 +10,7 @@
                             <h5 class="card-title">{{ $post->title }}</h5>
                             <p class="card-text">
                                 {{ $post->get_excerpt }}
-                                <a href="{{ router('post', $post) }}">Leer más</a>
+                                <a href="{{ route('post', $post) }}">Leer más</a>
                             </p>
                         </div>
                     </div>
