7bbc30d178802fec2ea2ede08b2eed1e8abfbfe7
Paul Cortes <paulkerg@gmail.com>
2020-04-20 08:40:47 -0500

    uso de Sluggable para el modelo Post

--- app/Post.php
+++ app/Post.php
@@ -2,9 +2,20 @@
 
 namespace App;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    use Sluggable;
+
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title',
+                'onUpdate' => true
+            ]
+        ];
+    }
 }
