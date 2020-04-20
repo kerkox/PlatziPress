38aae60ce0a527553a498b6011b01c8b77cd450b
Paul Cortes <paulkerg@gmail.com>
2020-04-20 13:43:33 -0500

    [Fix] ajustes

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,1 +1,21 @@
-<?php
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                @foreach($posts as $post)
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $post->title }}</h5>
+                            <p class="card-text">
+                                {{ $post->get_excerpt }}
+                                <a href="{{ router('post', $post) }}">Leer más</a>
+                            </p>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+@endsection
