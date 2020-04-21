ef58622f68838a3f8f41050ebc279aa7aa2da855
Paul Cortes <paulkerg@gmail.com>
2020-04-20 23:31:55 -0500

    modificacion en la creacion de la vista create para POST

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -14,7 +14,31 @@
                             </div>
                         @endif
 
-                        You are logged in!
+                            <form action="{{ route('posts.store') }}"
+                            method="POST"
+                            enctype="multipart/form-data">
+                                <div class="form-group">
+                                    <label>Titulo *</label>
+                                    <input type="text" name="title" class="form-control" required>
+                                </div>
+                                <div class="form-group">
+                                    <label>Imagen</label>
+                                    <input type="file" name="file" class="form-control-file">
+                                </div>
+                                <div class="form-group">
+                                    <label>Contenido *</label>
+                                    <textarea name="body" rows="6" class="form-control" required></textarea>
+                                </div>
+                                <div class="form-group">
+                                    <label>Contenido embebido</label>
+                                    <textarea name="iframe"  class="form-control"></textarea>
+                                </div>
+                                <div class="form-group">
+                                    @csrf
+                                    <input type="submit" value="Enviar" class="btn btn-sm btn-primary">
+                                </div>
+
+                            </form>
                     </div>
                 </div>
             </div>
