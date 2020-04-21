75134b2f1041d9e72e973a0be3828b9310cace85
Paul Cortes <paulkerg@gmail.com>
2020-04-21 13:29:36 -0500

    ajustes de formulario de edicion

--- app/Http/Controllers/Backend/PostController.php
+++ app/Http/Controllers/Backend/PostController.php
@@ -53,16 +53,6 @@ class PostController extends Controller
         return back()->with('status', 'Creado con éxito');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Post $post)
-    {
-        //
-    }
 
     /**
      * Show the form for editing the specified resource.
@@ -72,7 +62,7 @@ class PostController extends Controller
      */
     public function edit(Post $post)
     {
-        //
+        return view('posts.edit', compact('post'));
     }
 
     /**
