458198997620d394c418358966e349f6025e1022
Paul Cortes <paulkerg@gmail.com>
2020-04-20 22:58:29 -0500

    [Fix] ajuste en nombre de metodo

--- app/Http/Controllers/Backend/PostController.php
+++ app/Http/Controllers/Backend/PostController.php
@@ -15,7 +15,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::lastest()->get();
+        $posts = Post::latest()->get();
 
         return view('posts.index', compact($posts));
     }
